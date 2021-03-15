6610b479e916ec2b58f0915c06a3da1df1fafcb7
klueless-io <david@ideasmen.com.au>
2021-03-15 17:03:46 +1100

    first commit

---
+++ lib/k_builder/webpack5.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "webpack5/version"
+
+module KBuilder
+  module Webpack5
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
